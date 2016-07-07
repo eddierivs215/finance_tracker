837d2b8aab7ec54e64c86cb61165eb87c39a8277
Eddie Riv <eddierivs91@gmail.com>
2016-07-07 21:35:46 +0000

    change stock_id to integer

--- db/migrate/20160706004719_create_user_stocks.rb
+++ db/migrate/20160706004719_create_user_stocks.rb
@@ -2,7 +2,7 @@ class CreateUserStocks < ActiveRecord::Migration
   def change
     create_table :user_stocks do |t|
       t.integer :user_id
-      t.string :stock_id
+      t.integer :stock_id
       t.string :integer
 
       t.timestamps null: false
